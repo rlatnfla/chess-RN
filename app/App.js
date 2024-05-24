2679ac2dbc4e7d3e99263d6f5a2f8e93e8455e55
rlatnfla <rlatnfla0724@gmail.com>
2024-05-24 14:42:39 +0900

    knigt, bishop, rook, queen implemented.

--- app/App.js
+++ app/App.js
@@ -8,21 +8,72 @@ import {
 } from "react-native";
 import Board from "../components/Board.js";
 import { useEffect, useState } from "react";
-import { CanGo, ChessPiece, Pawn } from "../components/ChessPiece.js";
+import {
+  Bishop,
+  ChessPiece,
+  Knight,
+  Pawn,
+  Queen,
+  Rook,
+} from "../components/ChessPiece.js";
 
 export default function App() {
-  const initPosition = Array(8)
-    .fill(null)
-    .map(() =>
-      Array(8)
-        .fill(null)
-        .map(() => null)
-    );
-  initPosition[1][0] = new Pawn(2, 1, 0);
+  function initBoard() {
+    const board = Array(8)
+      .fill(null)
+      .map((r, rowIndex) => {
+        if (rowIndex === 1) {
+          return Array(8)
+            .fill(null)
+            .map((_, colIndex) => new Pawn(2, rowIndex, colIndex));
+        } else if (rowIndex === 6) {
+          return Array(8)
+            .fill(null)
+            .map((_, colIndex) => new Pawn(1, rowIndex, colIndex));
+        } else if (rowIndex === 0) {
+          return Array(8)
+            .fill(null)
+            .map((c, colIndex) => {
+              if (colIndex === 0 || colIndex === 7)
+                return new Rook(2, rowIndex, colIndex);
+              else if (colIndex === 1 || colIndex === 6)
+                return new Knight(2, rowIndex, colIndex);
+              else if (colIndex === 2 || colIndex === 5)
+                return new Bishop(2, rowIndex, colIndex);
+              else if (colIndex === 3) return new Queen(2, rowIndex, colIndex);
+              else return null;
+            });
+        } else if (rowIndex === 7) {
+          return Array(8)
+            .fill(null)
+            .map((c, colIndex) => {
+              if (colIndex === 0 || colIndex === 7)
+                return new Rook(1, rowIndex, colIndex);
+              else if (colIndex === 1 || colIndex === 6)
+                return new Knight(1, rowIndex, colIndex);
+              else if (colIndex === 2 || colIndex === 5)
+                return new Bishop(1, rowIndex, colIndex);
+              else if (colIndex === 3) return new Queen(1, rowIndex, colIndex);
+              else return null;
+            });
+        } else return Array(8).fill(null);
+      });
+
+    return board;
+  }
+
+  const initPosition = initBoard();
 
   const [position, setPosition] = useState(initPosition);
   const [selectedPiece, setSelectedPiece] = useState(null);
   const [possibleMoves, setPossibleMoves] = useState(null);
+  const [player, setPlayer] = useState(null);
+
+  console.log(player);
+
+  useEffect(() => {
+    setPlayer(1);
+  }, []);
 
   useEffect(() => {
     if (selectedPiece !== null) {
@@ -35,7 +86,10 @@ export default function App() {
     }
   }, [selectedPiece]);
 
-  console.log(possibleMoves);
+  // after board changed(when piece moved), change player
+  useEffect(() => {
+    setPlayer(player === 1 ? 2 : 1);
+  }, [position]);
 
   const onPiecePress = (row, col) => {
     const pressedPiece = position[row][col];
@@ -52,6 +106,8 @@ export default function App() {
         console.log("fuck");
         return;
       }
+    } else if (pressedPiece.player !== player) {
+      if (!selectedPiece) return;
     }
 
     if (selectedPiece) {
@@ -70,12 +126,14 @@ export default function App() {
           return false;
         })
       ) {
-        const newPosition = JSON.parse(JSON.stringify(position));
+        const newPosition = position.map((r, rowIndex) =>
+          r.map((c, colIndex) => c)
+        );
+        newPosition[selectedRow][selectedCol] = null;
         newPosition[row][col] = position[selectedRow][selectedCol].getCopy(
           row,
           col
         );
-        newPosition[selectedRow][selectedCol] = null;
 
         setPosition(newPosition);
         setSelectedPiece(null);
--- components/ChessPiece.js
+++ components/ChessPiece.js
@@ -11,6 +11,8 @@ export class ChessPiece {
   getCanMove() {}
 
   getCopy(row = this.row, col = this.col) {}
+
+  canCapture(row, col) {}
 }
 
 export class Pawn extends ChessPiece {
@@ -30,7 +32,6 @@ export class Pawn extends ChessPiece {
       row + direction < 8 &&
       position[row + direction][col] === null
     ) {
-      console.log(row + direction);
       moves.push([row + direction, col]);
     }
 
@@ -68,4 +69,220 @@ export class Pawn extends ChessPiece {
   getCopy(row = this.row, col = this.col) {
     return new Pawn(this.player, row, col);
   }
+}
+
+export class Rook extends ChessPiece {
+  getIcon() {
+    return this.player === 1 ? "♖" : "♜";
+  }
+
+  getCanMove(position) {
+    const moves = [];
+    const row = this.row;
+    const col = this.col;
+
+    for (let i = row - 1; i >= 0; i--) {
+      if (!position[i][col]) moves.push([i, col]);
+      else {
+        if (position[i][col].player === this.player) break;
+        else {
+          moves.push([i, col]);
+          break;
+        }
+      }
+    }
+    for (let i = row + 1; i < 8; i++) {
+      if (!position[i][col]) moves.push([i, col]);
+      else {
+        if (position[i][col].player === this.player) break;
+        else {
+          moves.push([i, col]);
+          break;
+        }
+      }
+    }
+    for (let i = col - 1; i >= 0; i--) {
+      if (!position[row][i]) moves.push([row, i]);
+      else {
+        if (position[row][i].player === this.player) break;
+        else {
+          moves.push([row, i]);
+          break;
+        }
+      }
+    }
+    for (let i = col + 1; i < 8; i++) {
+      if (!position[row][i]) moves.push([row, i]);
+      else {
+        if (position[row][i].player === this.player) break;
+        else {
+          moves.push([row, i]);
+          break;
+        }
+      }
+    }
+    console.log(moves);
+    return moves;
+  }
+
+  getCopy(row = this.row, col = this.col) {
+    return new Rook(this.player, row, col);
+  }
+}
+
+export class Knight extends ChessPiece {
+  getIcon() {
+    return this.player === 1 ? "♘" : "♞";
+  }
+
+  getCanMove(position) {
+    const moves = [];
+    const row = this.row;
+    const col = this.col;
+
+    const isValidMove = (targetRow, targetCol) => {
+      return (
+        targetRow >= 0 &&
+        targetRow < 8 &&
+        targetCol >= 0 &&
+        targetCol < 8 &&
+        (!position[targetRow][targetCol] ||
+          position[targetRow][targetCol].player !== this.player)
+      );
+    };
+
+    if (row - 2 >= 0) {
+      if (isValidMove(row - 2, col - 1)) moves.push([row - 2, col - 1]);
+      if (isValidMove(row - 2, col + 1)) moves.push([row - 2, col + 1]);
+    }
+    if (row + 2 < 8) {
+      if (isValidMove(row + 2, col - 1)) moves.push([row + 2, col - 1]);
+      if (isValidMove(row + 2, col + 1)) moves.push([row + 2, col + 1]);
+    }
+    if (col - 2 >= 0) {
+      if (isValidMove(row - 1, col - 2)) moves.push([row - 1, col - 2]);
+      if (isValidMove(row + 1, col - 2)) moves.push([row + 1, col - 2]);
+    }
+    if (col + 2 < 8) {
+      if (isValidMove(row - 1, col + 2)) moves.push([row - 1, col + 2]);
+      if (isValidMove(row + 1, col + 2)) moves.push([row + 1, col + 2]);
+    }
+
+    return moves;
+  }
+
+  getCopy(row = this.row, col = this.col) {
+    return new Knight(this.player, row, col);
+  }
+}
+
+export class Bishop extends ChessPiece {
+  getIcon() {
+    return this.player === 1 ? "♗" : "♝";
+  }
+
+  getCanMove(position) {
+    const moves = [];
+    const row = this.row;
+    const col = this.col;
+
+    // Check all four diagonal directions
+    const directions = [
+      [-1, -1], // top-left
+      [-1, 1], // top-right
+      [1, -1], // bottom-left
+      [1, 1], // bottom-right
+    ];
+
+    for (const [rowIncrement, colIncrement] of directions) {
+      let newRow = row + rowIncrement;
+      let newCol = col + colIncrement;
+
+      while (newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8) {
+        if (!position[newRow][newCol]) {
+          moves.push([newRow, newCol]);
+        } else {
+          if (position[newRow][newCol].player !== this.player) {
+            moves.push([newRow, newCol]);
+          }
+          break; // Stop if another piece is encountered
+        }
+        newRow += rowIncrement;
+        newCol += colIncrement;
+      }
+    }
+
+    return moves;
+  }
+
+  getCopy(row = this.row, col = this.col) {
+    return new Bishop(this.player, row, col);
+  }
+}
+
+export class Queen extends ChessPiece {
+  getIcon() {
+    return this.player === 1 ? "♕" : "♛";
+  }
+
+  getCanMove(position) {
+    const moves = [];
+    const row = this.row;
+    const col = this.col;
+
+    const isValidMove = (targetRow, targetCol) => {
+      return (
+        targetRow >= 0 &&
+        targetRow < 8 &&
+        targetCol >= 0 &&
+        targetCol < 8 &&
+        (!position[targetRow][targetCol] ||
+          position[targetRow][targetCol].player !== this.player)
+      );
+    };
+
+    // Directions for Queen: all 8 possible directions (vertical, horizontal, diagonal)
+    const directions = [
+      [-1, 0], // up
+      [1, 0], // down
+      [0, -1], // left
+      [0, 1], // right
+      [-1, -1], // top-left
+      [-1, 1], // top-right
+      [1, -1], // bottom-left
+      [1, 1], // bottom-right
+    ];
+
+    for (const [rowIncrement, colIncrement] of directions) {
+      let newRow = row + rowIncrement;
+      let newCol = col + colIncrement;
+
+      while (newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8) {
+        if (!position[newRow][newCol]) {
+          moves.push([newRow, newCol]);
+        } else {
+          if (position[newRow][newCol].player !== this.player) {
+            moves.push([newRow, newCol]);
+          }
+          break; // Stop if another piece is encountered
+        }
+        newRow += rowIncrement;
+        newCol += colIncrement;
+      }
+    }
+
+    return moves;
+  }
+
+  getCopy(row = this.row, col = this.col) {
+    return new Queen(this.player, row, col);
+  }
+}
+
+export class King extends ChessPiece {
+  getIcon() {
+    return this.player === 1 ? "♔" : "♚";
+  }
+
+  getCanMove(position) {}
 }
